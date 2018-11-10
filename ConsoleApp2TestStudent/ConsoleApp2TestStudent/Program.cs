51a4baacdf9264bb3e242782bfa038b5e5fd992e
wedwer666 <mitriucmaria@gmail.com>
2018-11-10 15:57:17 +0200

    Add files via upload

---
+++ ConsoleApp2TestStudent/ConsoleApp2TestStudent/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace ConsoleApp2TestStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("teststudent");
+
+            var collection = database.GetCollection<BsonDocument>("student");
+            Console.WriteLine("Data from Mongodb DATABASE");
+            collection.Find(new BsonDocument()).ForEachAsync(X => Console.WriteLine(X));
+            Console.Read();
+        }
+    }
+}
